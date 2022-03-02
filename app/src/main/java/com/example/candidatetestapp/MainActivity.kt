436a68a40822200a16753b80a592ca64ec2bcc2e
cr1m1 <kalysbek_z@auca.kg>
2022-03-02 22:11:45 +0600

    made viewpager, but im going to delete it and make bottom navigation

--- app/src/main/java/com/example/candidatetestapp/MainActivity.kt
+++ app/src/main/java/com/example/candidatetestapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.candidatetestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
+    private lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabLayout = findViewById<TabLayout>(R.id.main_tablayout)
+        viewPager = findViewById(R.id.main_viewpager)
+        viewPager.offscreenPageLimit = 2
+
+        mainViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = mainViewPagerAdapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position){
+                0 -> {
+                    tab.text = "Greeter"
+                }
+                1 -> {
+                    tab.text = "Gallery"
+                }
+            }
+        }
     }
 }
