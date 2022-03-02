67c8584177e8348dc77cbac04f3379f6672170e3
cr1m1 <kalysbek_z@auca.kg>
2022-03-02 23:47:50 +0600

    finished bottom navigation

--- app/src/main/java/com/example/candidatetestapp/MainActivity.kt
+++ app/src/main/java/com/example/candidatetestapp/MainActivity.kt
@@ -2,34 +2,41 @@ package com.example.candidatetestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mainViewPagerAdapter: MainViewPagerAdapter
-    private lateinit var viewPager: ViewPager2
+    private lateinit var bottomNavigationView: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tabLayout = findViewById<TabLayout>(R.id.main_tablayout)
-        viewPager = findViewById(R.id.main_viewpager)
-        viewPager.offscreenPageLimit = 2
+        bottomNavigationView = findViewById(R.id.mainBottomNav)
+        val actionBar = supportActionBar
+        actionBar?.title = "Profile"
+        openFragment(GreeterFragment())
 
-        mainViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager, lifecycle)
-        viewPager.adapter = mainViewPagerAdapter
-
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            when (position){
-                0 -> {
-                    tab.text = "Greeter"
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.greeter_item -> {
+                    actionBar?.title = "Profile"
+                    openFragment(GreeterFragment())
+                    true
                 }
-                1 -> {
-                    tab.text = "Gallery"
+                R.id.gallery_item -> {
+                    actionBar?.title = "Gallery"
+                    openFragment(GalleryFragment())
+                    true
                 }
+                else -> false
             }
         }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, fragment)
+            .commit()
     }
 }
